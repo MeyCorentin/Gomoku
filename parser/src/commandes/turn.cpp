af82fe489222d798a7fe4cdb2c46d7cb10ce15ac
MeyCorentin <corentin.mey@epitech.eu>
2023-11-21 16:29:09 +0100

    [ADD] working turn

--- parser/src/commandes/turn.cpp
+++ parser/src/commandes/turn.cpp
@@ -5,8 +5,9 @@ void Parser::C_turn(Bitboard &arg_board, std::string &arg_input)
     MinMax min_max(&arg_board);
     std::istringstream iss(arg_input);
     std::string cmd;
+    char delimiter;
     int x, y;
-    iss >> cmd >> x >> y;
+    iss >> cmd >> x >> delimiter >> y;
     arg_board.setBit(std::make_pair(x, y), 0);
     std::pair<int, int> brain_play = min_max.playTurn();
     arg_board.setBit(brain_play , 1);
